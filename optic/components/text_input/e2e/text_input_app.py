f5b8836993d2ed9d8aebd862d1b7cba7ad11239b
Will Chen <willchen90@gmail.com>
2023-11-24 23:22:25 -0800

    WIP: text input

--- optic/components/text_input/e2e/text_input_app.py
+++ optic/components/text_input/e2e/text_input_app.py
@@ -1,6 +1,19 @@
 import optic as op
 
 
+@op.stateclass
+class State:
+    string: str = "a"
+
+
+@op.on(op.ChangeEvent)
+def change(action: op.ChangeEvent):
+    state = op.state(State)
+    state.string = action.value
+
+
 @op.page(path="/components/text_input/e2e/text_input_app")
 def app():
-    op.text_input(label="Hello, world!")
+    op.text_input(label="Hello, world!", on_change=change)
+    state = op.state(State)
+    op.text(text=state.string)
