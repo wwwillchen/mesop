b9742a0b1da4e85b3fe1d84b37dc8d40c5dfa5f2
Will Chen <willchen90@gmail.com>
2023-12-07 15:08:29 -0800

    prototype: send_file

--- optic/server/prod_server.py
+++ optic/server/prod_server.py
@@ -1,24 +1,27 @@
 import os
-from flask import send_from_directory
+from flask import send_file
 from .server import app, port
 from rules_python.python.runfiles import runfiles  # type: ignore
+from werkzeug.utils import safe_join
 
 
-def get_path():
-    return runfiles.Create().Rlocation("optic/web/src/app/prod/web_package")
+def get_path(path: str) -> str:
+    p = runfiles.Create().Rlocation(safe_join("optic/web/src/app", path))
+    print("**path", p)
+    return p
 
 
 @app.route("/")
 def serve_root():
-    return send_from_directory(get_path(), "index.html")
+    return send_file(get_path("index.html"))
 
 
 @app.route("/<path:path>")
 def serve_file(path: str):
     if is_file_path(path):
-        return send_from_directory(get_path(), path)
+        return send_file(get_path(path))
     else:
-        return send_from_directory(get_path(), "index.html")
+        return send_file(get_path("index.html"))
 
 
 def run():
